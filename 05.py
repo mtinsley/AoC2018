2a278f9f516e9717b545754f1022c741c99c27c9
Mathew Tinsley <mtinsley@users.noreply.github.com>
2018-12-04 22:27:47 -0800

    Add comments for day 5

--- 05.py
+++ 05.py
@@ -3,13 +3,17 @@ from sys import stdin
 
 
 def day_five_part_one(data):
+    """
+    Remove repeated, alternating uppercase/lowercase characters until none remain in data.
+    Return the length of the resulting data
+    """
     last_len = len(data)
     while True:
         for lower in ascii_lowercase:
             upper = lower.upper()
             data = data.replace(lower + upper, '').replace(upper + lower, '')
         if last_len == len(data):
-            break
+            break  # Break if the data wasn't changed in this pass
 
         last_len = len(data)
 
@@ -17,6 +21,9 @@ def day_five_part_one(data):
 
 
 def day_five_part_two(data):
+    """
+    For each letter, determine the length of the resulting data if that letter is removed from the string (upper and lowercase)
+    """
     lengths = {}
     for lower in ascii_lowercase:
         upper = lower.upper()
