fbeca4ea362d5eb451990a27e2de976e0970b86f
Mathew Tinsley <mtinsley@users.noreply.github.com>
2018-12-09 23:03:50 -0800

    Add comments

--- 10.py
+++ 10.py
@@ -12,20 +12,24 @@ def parse_line(line):
 
 
 def new_position(xy, vxy):
+    """
+    Compute the new position for the given starting position (x, y) and velocity (vx, vy)
+    """
     return xy[0] + vxy[0], xy[1] + vxy[1]
 
 
 def write_messages(lines):
     grid = defaultdict(set)
+    # Parse coordinates and velocities from lines
     for line in lines:
         x, y, vx, vy = parse_line(line)
         grid[(x, y)].add((vx, vy))
 
-    in_threshold = False
+    in_threshold = False  # Indicates the point where files should start to be written (max_x - min_x)
     file_written = False
     iteration = 0
     threshold = 100
-    while not (file_written and not in_threshold):
+    while not file_written or in_threshold:
         iteration += 1
         new_grid = defaultdict(set)
         max_x = None
@@ -41,8 +45,12 @@ def write_messages(lines):
                 min_y = min(min_y, xy[1]) if min_y is not None else xy[1]
         grid = new_grid
         if (max_x - min_x) < threshold:
+            # Start writing files when max_x - min_x is within the threshold
+            # This reduces the number of files written to make it easier to locate the message
             in_threshold = True
             with open('out/' + str(iteration) + '.txt', 'w') as f:
+                # Write characters between min/max y and min/max x
+                # Write a '#' if x,y corresponds to a coordinate in the message, write a space ' ' otherwise
                 for y in range(min_y - 1, max_y + 1):
                     line = ''
                     for x in range(min_x - 1, max_x + 1):
@@ -55,3 +63,5 @@ def write_messages(lines):
 
 inp = stdin_get_lines()
 write_messages(inp)
+# The message (part 1) should be in one of the files written to the out directory
+# The number of seconds required to reach that message (part 2) will be the name of the file
